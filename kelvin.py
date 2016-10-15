0d75d829104665edf9ec8e127dc20eab467010cb
Seba <dese@student.chalmers.se>
2016-10-15 05:44:20 +0200

    xddd

--- kelvin.py
+++ kelvin.py
@@ -14,6 +14,8 @@ dataset = [
                 ('xau',  CLOSE),                
             ];
 
+CORRECTION = 0;
+
 def predictFor(clf, day):
     teX = getdata(dataset, day-10, day, noMemes=True);
     prd = prn.NNOut(teX, clf);
@@ -22,30 +24,38 @@ def predictFor(clf, day):
     return o, p;
 
 if (__name__ == "__main__"):
-    TRIALS = 1;
+    pss = [];
+    TRIALS = 10;
     for i in range(TRIALS):
-        CO = 1257 if PREDICT else 1000;
-        clf = prn.CreateNN([len(dataset),15,1],dIn=[0],dIntern=[1,2],dOut=[])
+        CO = 1257 if PREDICT else 1100;
+        clf = prn.CreateNN([len(dataset),10,1],dIn=[0],dIntern=[1,2],dOut=[])
 
         trX, trY = getdata(dataset, 0, CO);
 
-        clf = prn.train_LM(trX, trY, clf,verbose=True,k_max=20,E_stop=0.1)
+        clf = prn.train_LM(trX, trY, clf,verbose=True,k_max=10,E_stop=0.1)
 
+        xd = [(predictFor(clf, i), getCloseFor(i)) for i in range(1207, 1257)];
+
+        CORRECTION = -sum(map(lambda x: x[0][1] - x[1], xd))/50;
+        print(CORRECTION);
         if (PREDICT):
             o, p = predictFor(clf, 1258);
             print(o, p);
+            pss.append(p+CORRECTION);
         else:
             cr, inc, profit = 0, 0, 0;
             os = [];
             ps = [];
             az = [];
-            for i in range(CO, 1257):
-                o, p = predictFor(clf, i)
-                a = getCloseFor(i)
+            for i in range(CO, CO+100):
+                o, p = predictFor(clf, i);
+                a = getCloseFor(i);
+                p = p + CORRECTION;
                 os.append(o);
                 ps.append(p);
                 az.append(a);
-                #print(o, p, a);
+                a = a*0.9995;
+                #   print(o, p, a);
                 if (p > o):
                     profit += a - o;
                     if (a > o):
@@ -58,3 +68,6 @@ if (__name__ == "__main__"):
             plt.plot(ps);
             plt.plot(az);
             plt.show();
+
+    pss.sort();
+    print(pss);
