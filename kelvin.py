1c0f5702db3e24150b27b37939c559baabaee467
Seba <dese@student.chalmers.se>
2016-10-17 22:55:27 +0200

    kappapride

--- kelvin.py
+++ kelvin.py
@@ -5,6 +5,7 @@ import pyrenn as prn
 
 PREDICT = False;
 DAYSOFDATA = 1258;
+RAMP = 15;
 dataset = [
                 ('gspc', OPEN),
                 ('gspc', HIGH),
@@ -17,26 +18,26 @@ dataset = [
 CORRECTION = 0;
 
 def predictFor(clf, day):
-    teX = getdata(dataset, day-10, day, noMemes=True);
+    teX = getdata(dataset, day-RAMP, day, noMemes=True);
     prd = prn.NNOut(teX, clf);
-    o = rescale(teX[3][9]);
+    o = rescale(teX[3][RAMP-1]);
     p = rescale(prd[-1:][0]);
     return o, p;
 
 if (__name__ == "__main__"):
     pss = [];
-    TRIALS = 10;
+    TRIALS = 1;
     for i in range(TRIALS):
         CO = 1257 if PREDICT else 1100;
-        clf = prn.CreateNN([len(dataset),10,1],dIn=[0],dIntern=[1,2],dOut=[])
+        clf = prn.CreateNN([len(dataset),10,1],dIn=[1,2,3],dIntern=[],dOut=[])
 
         trX, trY = getdata(dataset, 0, CO);
 
-        clf = prn.train_LM(trX, trY, clf,verbose=True,k_max=10,E_stop=0.1)
+        clf = prn.train_LM(trX, trY, clf,verbose=True,k_max=15,E_stop=0.1)
 
         xd = [(predictFor(clf, i), getCloseFor(i)) for i in range(1207, 1257)];
 
-        CORRECTION = -sum(map(lambda x: x[0][1] - x[1], xd))/50;
+        CORRECTION = 0-sum(map(lambda x: x[0][1] - x[1], xd))/50;
         print(CORRECTION);
         if (PREDICT):
             o, p = predictFor(clf, 1258);
@@ -47,6 +48,7 @@ if (__name__ == "__main__"):
             os = [];
             ps = [];
             az = [];
+            ds = [];
             for i in range(CO, CO+100):
                 o, p = predictFor(clf, i);
                 a = getCloseFor(i);
@@ -55,6 +57,7 @@ if (__name__ == "__main__"):
                 ps.append(p);
                 az.append(a);
                 a = a-0.5;
+                ds.append(p-a);
                 #   print(o, p, a);
                 if (p > o):
                     profit += a - o;
@@ -65,8 +68,9 @@ if (__name__ == "__main__"):
             print(cr, inc);
             print(profit);
             #plt.plot(os);
-            plt.plot(ps);
-            plt.plot(az);
+            #plt.plot(ps);
+            #plt.plot(az);
+            plt.plot(ds);
             plt.show();
 
     pss.sort();
